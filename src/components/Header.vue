a4f7bec6969953040d963717e8ba25a04d791de4
wangxiujuni <1091242543@qq.com>
2019-05-29 01:18:56 +0800

    1

---
+++ src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <mu-appbar color="primary">
+      <mu-button icon slot="left" @click.stop="showDrawer">
+        <mu-icon value="menu"></mu-icon>
+      </mu-button>Title
+      <mu-menu slot="right">
+        <mu-button flat>MENU</mu-button>
+        <mu-list slot="content">
+          <mu-list-item button>
+            <mu-list-item-content>
+              <mu-list-item-title>Menu Item 1</mu-list-item-title>
+            </mu-list-item-content>
+          </mu-list-item>
+          <mu-list-item button>
+            <mu-list-item-content>
+              <mu-list-item-title>Menu Item 2</mu-list-item-title>
+            </mu-list-item-content>
+          </mu-list-item>
+        </mu-list>
+      </mu-menu>
+    </mu-appbar>
+    <mu-drawer :open.sync="open" :docked="true" :width="width">
+      <mu-list>
+        <mu-list-item button>
+          <mu-list-item-title>Menu Item 1</mu-list-item-title>
+        </mu-list-item>
+        <mu-list-item button>
+          <mu-list-item-title>Menu Item 2</mu-list-item-title>
+        </mu-list-item>
+        <mu-list-item @click="open = false" button>
+          <mu-list-item-title>Close</mu-list-item-title>
+        </mu-list-item>
+      </mu-list>
+    </mu-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      open: false,
+      width:0
+    }
+  },
+  mounted(){
+    this.width=window.innerWidth*0.45
+  },
+  methods: {
+    showDrawer() {
+      this.open = true
+      document.onclick = () => {
+        this.open = false
+        document.onclick = null
+      }
+    }
+  }
+}
+</script>
+
+<style module lang="postcss">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'muse-ui/lib/styles/base.less'
+import { AppBar,Menu,Button,List,Icon,Drawer } from 'muse-ui'
+import 'muse-ui/lib/styles/theme.less'
+
+Vue.use(AppBar).use(Menu).use(Button).use(List).use(Icon).use(Drawer)
 
 Vue.config.productionTip = false
 
